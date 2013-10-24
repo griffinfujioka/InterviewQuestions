d9c9688086be27ac75c26348806d7c926d3d3287
Griffin Fujioka <fujiokag@hotmail.com>
2013-10-23 22:25:19 -0700

    Did the distribution problem
    Using C++ STL Vectors

--- FillArrayWithGivenDistribution/Main.cpp
+++ FillArrayWithGivenDistribution/Main.cpp
@@ -12,3 +12,54 @@
 //	- Generate 50 random numbers with max=500, add 500 to each random number and populate indices 950-999
 //	- Write a helper function with a prototype looking like:
 //			int[] GenerateNumber(int n, int min, int max)		// Generate n numbers such that each is between min and max 
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+#include <vector>
+using namespace std;
+
+#define MAX_SIZE 1000
+
+vector<int> GenerateNumbers(int n, int min, int max)
+{
+	const int size = n; 
+	vector<int> vec(n); 
+	int i = 0; 
+
+	int num;
+	srand(0); 
+	while(i < n)
+	{
+		num = rand() % (max - min + 1);			// generate a random number between 1 and max 
+		num += min + 1; 
+		vec[i] = num; 
+		i++; 
+	}
+
+
+	return vec;
+}
+
+int main()
+{
+	int arr[MAX_SIZE]; 
+	int i = 0, j = 0; 
+	int num; 
+
+	vector<int> test = GenerateNumbers(800, 1, 100); 
+	vector<int> test2 = GenerateNumbers(150, 101, 500); 
+	test.insert(test.end(), test2.begin(), test2.end()); 
+
+	vector<int> test3 = GenerateNumbers(50, 501, 1000); 
+
+	test.insert(test.end(), test3.begin(), test3.end()); 
+
+
+	while(i < MAX_SIZE)
+	{
+		cout << test[i] << endl; 
+		i++;
+	}
+
+	return 0; 
+}
