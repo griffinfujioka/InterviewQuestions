4e55da608de42f3f3b73f737a01b4654360a734a
Griffin Fujioka <fujiokag@hotmail.com>
2013-11-11 23:47:14 -0800

    save the streak commit

---
+++ GreatestSubsequence/Main.cpp
@@ -0,0 +1,15 @@
+// Given a list of integers, determine the start index of the sequence of 5 numbers
+// whose sum is greatest
+// Algorithm: Approach it like you're looking for the maximum in the array
+// Start and index 0 and assume this results in the greatest subsequence
+// Iterate through the array checking the subsequence for each index and update
+// the start index if the a subsequence is found which results in a greater sum 
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+
+	return 0; 
+}
